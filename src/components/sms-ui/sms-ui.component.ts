b2e52880aef922b99ff680fe675c8226b16124da
Zahed75 <zahedhasan.cs@gmail.com>
2025-03-06 00:15:31 +0600

    OTP send to the mobile done

--- src/components/sms-ui/sms-ui.component.ts
+++ src/components/sms-ui/sms-ui.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
+import {SmsUIService} from '../../services/smsUI/sms-ui.service';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-sms-ui',
   templateUrl: './sms-ui.component.html',
+  imports: [
+    FormsModule
+  ],
   styleUrl: './sms-ui.component.css'
 })
 export class SmsUIComponent implements OnInit {
   captchaCode: string = ''; // Initialize with a default value
+  purchase: any = {
+    customer_name: '',
+    mobile_no: '',
+    product_code: '',
+    invoice_no: ''
+  };
 
-  constructor() {
+
+  constructor(
+    private smsUIService: SmsUIService
+  ) {
     this.generateCaptcha();
   }
+
+  router = inject(Router);
 
   ngOnInit() {
   }
@@ -25,6 +42,33 @@ export class SmsUIComponent implements OnInit {
     this.captchaCode = result;
   }
 
+
+  onSubmit(): void {
+    if (this.validateForm()) {
+      this.smsUIService.checkPurchase(this.purchase).subscribe({
+        next: (response) => {
+          console.log('API Response:', response);
+          alert('OTP sent successfully!');
+          this.router.navigate(['/verification']); // Navigate to the verification page
+        },
+        error: (error) => {
+          console.error('API Error:', error);
+          alert('Failed to send OTP. Please try again.');
+        }
+      });
+    } else {
+      alert('Please fill out all fields and agree to the terms and conditions.');
+    }
+  }
+
+  validateForm(): boolean {
+    return (
+      this.purchase.customer_name &&
+      this.purchase.mobile_no &&
+      this.purchase.product_code &&
+      this.purchase.invoice_no
+    );
+  }
 
 
 }
--- src/services/smsUI/sms-ui.service.ts
+++ src/services/smsUI/sms-ui.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../enviornments/enviornment';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SmsUIService {
 
-  constructor() { }
+  private checkPurchaseURL =`${environment.apiBaseUrl}/sms/api/generate-otp/`
+  private http = inject(HttpClient);
+
+
+
+  checkPurchase(customer:any):Observable<any>{
+    return this.http.post<{message:string}>(this.checkPurchaseURL,customer).pipe(
+      map((response)=>response)
+    )
+  }
+
+
 }
