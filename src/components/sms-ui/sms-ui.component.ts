6461664b1c4603817f24091162d52dcba780202a
Zahed75 <zahedhasan.cs@gmail.com>
2025-03-05 23:00:53 +0600

    Interceptor added

--- src/components/sms-ui/sms-ui.component.ts
+++ src/components/sms-ui/sms-ui.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sms-ui',
-  imports: [],
   templateUrl: './sms-ui.component.html',
   styleUrl: './sms-ui.component.css'
 })
-export class SmsUIComponent {
+export class SmsUIComponent implements OnInit {
+  captchaCode: string = ''; // Initialize with a default value
+
+  constructor() {
+    this.generateCaptcha();
+  }
+
+  ngOnInit() {
+  }
+
+  generateCaptcha(): void {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const length = 6;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    this.captchaCode = result;
+  }
+
+
 
 }
